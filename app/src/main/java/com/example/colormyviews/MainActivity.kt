b0da31f467f9d14858cc51afc3f41719eb1c04f9
VasVL <sobakagman.com@gmail.com>
2023-01-07 21:30:34 +0300

    2.29 Add Buttons From each handle we can only create one constraint + using Custom colors

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -4,26 +4,41 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var boxOneText: TextView
+    private lateinit var boxTwoText: TextView
+    private lateinit var boxThreeText: TextView
+    private lateinit var boxFourText: TextView
+    private lateinit var boxFiveText: TextView
+    private lateinit var constraintLayout: View
+    private lateinit var redButton: Button
+    private lateinit var yellowButton: Button
+    private lateinit var greenButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        boxOneText = findViewById(R.id.box_one_text)
+        boxTwoText = findViewById(R.id.box_two_text)
+        boxThreeText = findViewById(R.id.box_three_text)
+        boxFourText = findViewById(R.id.box_four_text)
+        boxFiveText = findViewById(R.id.box_five_text)
+        constraintLayout = findViewById(R.id.constraint_layout)
+        redButton = findViewById(R.id.red_button)
+        yellowButton = findViewById(R.id.yellow_button)
+        greenButton = findViewById(R.id.green_button)
         setListeners()
     }
 
     private fun setListeners() {
-        val boxOneText = findViewById<TextView>(R.id.box_one_text)
-        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
-        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
-        val boxFourText = findViewById<TextView>(R.id.box_four_text)
-        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
-        val constraintLayout = findViewById<View>(R.id.constraint_layout)
 
         val clickableViews: List<View> = listOf(
-            boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, constraintLayout
+            boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, constraintLayout,
+            redButton, yellowButton, greenButton
         )
 
         for (item in clickableViews) {
@@ -42,6 +57,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> boxFiveText.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
